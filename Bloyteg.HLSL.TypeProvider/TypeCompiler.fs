8e86253bffcd06d9b3004bf4b93876f120dc442b
Mr. Byte <bytemr@gmail.com>
2015-01-15 21:36:04 -0700

    Added a TypeCompiler module that provides facilities to compile C# types.

---
+++ Bloyteg.HLSL.TypeProvider/TypeCompiler.fs
@@ -0,0 +1,24 @@
+﻿module internal TypeCompiler
+
+open System
+open System.IO
+open System.Reflection
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp
+
+type AssemblyResult =
+| Success of byte[]
+| Failure of string seq
+
+let compileAssembly (name: string) (references: Assembly seq) (code: string) : AssemblyResult =
+    use outputStream = new MemoryStream()
+    let syntaxTree = CSharpSyntaxTree.ParseText(code)
+    let compilationResult = CSharpCompilation.Create(name,
+                                                     syntaxTrees = [syntaxTree],
+                                                     references = (references |> Seq.map MetadataReference.CreateFromAssembly),
+                                                     options = CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                                             .Emit(outputStream)
+    if compilationResult.Success then
+        Success(outputStream.GetBuffer())
+    else
+        Failure(compilationResult.Diagnostics |> Seq.map Convert.ToString)
